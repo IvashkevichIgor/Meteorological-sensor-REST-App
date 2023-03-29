08e8272676368b0ef982a1777a1345c237182555
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-03-29 18:19:41 +0300

    add one-to-many relation field

--- src/main/java/ru/ivashkevich/meteorological_sensor_rest/models/Sensor.java
+++ src/main/java/ru/ivashkevich/meteorological_sensor_rest/models/Sensor.java
@@ -5,6 +5,7 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.Size;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
 @Table(name = "sensor")
@@ -23,7 +24,8 @@ public class Sensor {
     @Column(name = "registered_at")
     private LocalDateTime registeredAt;
 
-
+    @OneToMany(mappedBy = "sensor")
+    private List<Measurement> measurements;
 
     public int getId() {
         return id;
@@ -47,5 +49,13 @@ public class Sensor {
 
     public void setRegisteredAt(LocalDateTime registeredAt) {
         this.registeredAt = registeredAt;
+    }
+
+    public List<Measurement> getMeasurements() {
+        return measurements;
+    }
+
+    public void setMeasurements(List<Measurement> measurements) {
+        this.measurements = measurements;
     }
 }
