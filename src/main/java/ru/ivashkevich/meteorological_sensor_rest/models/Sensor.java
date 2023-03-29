8f6c1e6729155fb4fc1b7ac0a98b925733b8f118
Igor Ivashkevich <ivashkevich.java@gmail.com>
2023-03-29 17:00:01 +0300

    add Sensor entity class without one-to-many relation field

---
+++ src/main/java/ru/ivashkevich/meteorological_sensor_rest/models/Sensor.java
@@ -0,0 +1,51 @@
+package ru.ivashkevich.meteorological_sensor_rest.models;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.Size;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "sensor")
+public class Sensor {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private int id;
+
+    @NotEmpty(message = "Name should not be empty")
+    @Size(min = 3, max = 30, message = "Name should be between 3 and 30 characters")
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "registered_at")
+    private LocalDateTime registeredAt;
+
+
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public LocalDateTime getRegisteredAt() {
+        return registeredAt;
+    }
+
+    public void setRegisteredAt(LocalDateTime registeredAt) {
+        this.registeredAt = registeredAt;
+    }
+}
